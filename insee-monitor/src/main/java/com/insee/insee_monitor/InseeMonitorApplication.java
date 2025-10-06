a728738e96e0d0d46216cd9d6f3789624942139a
Supun Dhananji <ksupundhananji@gmail.com>
2025-10-06 22:34:09 +0530

    Setup the project

---
+++ insee-monitor/src/main/java/com/insee/insee_monitor/InseeMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.insee.insee_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InseeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InseeMonitorApplication.class, args);
+	}
+
+}
---
+++ insee-monitor/src/test/java/com/insee/insee_monitor/InseeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insee.insee_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InseeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
