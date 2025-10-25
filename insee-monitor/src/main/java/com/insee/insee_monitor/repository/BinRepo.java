3e970568629b92f8d7425370b58b7414040fadff
Supun Dhananji <ksupundhananji@gmail.com>
2025-10-25 20:04:11 +0530

    repo filr for bins

--- insee-monitor/src/main/java/com/insee/insee_monitor/repository/BinRepo.java
+++ insee-monitor/src/main/java/com/insee/insee_monitor/repository/BinRepo.java
@@ -1,5 +1,10 @@
 package com.insee.insee_monitor.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.insee.insee_monitor.entity.Bin;
+import java.util.Optional;
 
-public class BinRepo {
+public interface BinRepo extends JpaRepository<Bin, Long >{
     
+
+    Optional<Bin>finfByEmail(String binNumber);
 }
